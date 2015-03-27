f36e8bf91a91b17c4e1d77fda097cd518b80bd23
Johannes Raggam <raggam-nl@adm.at>
2015-03-27 22:33:52 +0100

    gruntconfig including sed+copy tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,87 @@
-/* globals module:true */
+// Grunt software build task definitions.
+/* global module:true */
 module.exports = function(grunt) {
   'use strict';
 
-  // Get mockup-core's grunt infrastructure
-  var MockupGrunt = require('./bower_components/mockup-core/js/grunt');
-  // Include the project's RequireJS configuration
-  var requirejsOptions = require('./js/config');
+  var MockupGrunt = require('./bower_components/mockup-core/js/grunt'),
+      requirejsOptions = require('./js/config'),
+      mockup = new MockupGrunt(requirejsOptions);
 
-  // Create a new insance of the Mockup grunt task suite.
-  var mockup = new MockupGrunt(requirejsOptions);
-  // list of resources, which should be included for the documentation.
-  var docsExtraIncludes = [];
+  var dest_path = 'build/',
+      bundle_name = 'leaflet',
+      res_path = '++resource++mockup-leaflet/';
 
-  for (var i = 0; i < mockup.patterns.length; i = i + 1) {
-    if (mockup.patterns[i].indexOf('-url') === -1) {
-      docsExtraIncludes.push(mockup.patterns[i]);
-      docsExtraIncludes.push('text!' + requirejsOptions.paths[mockup.patterns[i]] + '.js');
+  mockup.registerBundle(
+    'leaflet', {
+      copy: {
+        leaflet: {  // copy must have an entry with name of the bundle
+          files: [
+            {
+              expand: true,
+              cwd: 'bower_components/leaflet/dist/images/',
+              src: '*',
+              dest: dest_path,
+              rename: function(dest, src) { return dest + bundle_name + '-' + src; }
+            },
+            {
+              expand: true,
+              cwd: 'bower_components/Leaflet.fullscreen/dist/',
+              src: '*.png',
+              dest: dest_path,
+              rename: function(dest, src) { return dest + bundle_name + '-' + src; }
+            }
+          ]
+        }
+      },
+      sed: {  // sed tasks must begin with name of bundle
+        'leaflet-1': {
+          path: dest_path,
+          recursive: true,
+          pattern: 'images/layers-2x.png',
+          replacement: res_path + bundle_name + '-layers-2x.png'
+        },
+        'leaflet-2': {
+          path: dest_path,
+          recursive: true,
+          pattern: 'images/layers.png',
+          replacement: res_path + bundle_name + '-layers.png'
+        },
+        'leaflet-3': {
+          path: dest_path,
+          recursive: true,
+          pattern: 'images/marker-icon-2x.png',
+          replacement: res_path + bundle_name + '-marker-icon-2x.png'
+        },
+        'leaflet-4': {
+          path: dest_path,
+          recursive: true,
+          pattern: 'images/marker-icon.png',
+          replacement: res_path + bundle_name + '-marker-icon.png'
+        },
+        'leaflet-5': {
+          path: dest_path,
+          recursive: true,
+          pattern: 'images/marker-shadow.png',
+          replacement: res_path + bundle_name + '-marker-shadow.png'
+        },
+        'leaflet-fullscreen-1': {
+          path: dest_path,
+          recursive: true,
+          pattern: 'fullscreen.png',
+          replacement: res_path + bundle_name + '-fullscreen.png'
+        },
+        'leaflet-fullscreen-2': {
+          path: dest_path,
+          recursive: true,
+          pattern: 'fullscreen@2x.png',
+          replacement: res_path + bundle_name + '-fullscreen@2x.png'
+        },
+      }
+    },
+    {
+      path: dest_path,
+      url: res_path + bundle_name
     }
-  }
-
-  // Register the docs bundle with some custom config.
-  mockup.registerBundle('docs',
-    {less: {options: {modifyVars: {bowerPath: '"bower_components/"' }}}},
-    {extraInclude: docsExtraIncludes},
-    ['less',]
   );
-  // Register the leaflet pattern
-  mockup.registerBundle('leaflet');
-  // initialize grunt.
   mockup.initGrunt(grunt);
 };
